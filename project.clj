f604c48d8ae2cadc9d5a5cb77a5b9f1cf2aef45a
Crispin Wellington <retrogradeorbit@gmail.com>
2020-05-08 22:57:46 +0800

    deps

--- project.clj
+++ project.clj
@@ -3,5 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojure "1.10.2-alpha1"]
+                 [borkdude/edamame "0.0.11-alpha.9"]
+                 [borkdude/sci "0.0.13-alpha.19"]
+                 ]
   :repl-options {:init-ns sci-nrepl.core})
