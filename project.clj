093f890c062d7e1ed4478245065058a0e3c8ab3b
Crispin Wellington <retrogradeorbit@gmail.com>
2020-05-13 10:32:37 +0800

    license

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject sci-nrepl "0.1.0-SNAPSHOT"
   :description "sci nREPL module"
   :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :license {:name "EPL-1.0"
+            :url "https://www.eclipse.org/legal/epl-1.0/"}
   :dependencies [[org.clojure/clojure "1.10.2-alpha1"]
                  [nrepl/bencode "1.1.0"]
                  [borkdude/edamame "0.0.11-alpha.9"]
