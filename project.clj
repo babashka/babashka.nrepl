903489fcb5d23fe1ebf4f6e1e321419ed3739268
Michiel Borkent <michielborkent@gmail.com>
2020-05-17 16:24:30 +0200

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject babashka/babashka.nrepl "0.0.2"
+(defproject babashka/babashka.nrepl "0.0.3-SNAPSHOT"
   :description "babashka nREPL module"
   :url "https://github.com/babashka/babashka.nrepl"
   :scm {:name "git"
