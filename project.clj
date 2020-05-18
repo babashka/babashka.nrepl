006f29e38120000cdbfe5586dd799cbc9fd13389
Crispin Wellington <retrogradeorbit@gmail.com>
2020-05-18 22:52:52 +0800

    release v0.0.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject babashka/babashka.nrepl "0.0.3-SNAPSHOT"
+(defproject babashka/babashka.nrepl "0.0.3"
   :description "babashka nREPL module"
   :url "https://github.com/babashka/babashka.nrepl"
   :scm {:name "git"
