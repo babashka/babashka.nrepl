2d1f06b0431e51735075a2a678be534efa5777d4
Michiel Borkent <michielborkent@gmail.com>
2020-06-15 10:38:31 +0200

    Quote query for regex

--- src/babashka/nrepl/impl/server.clj
+++ src/babashka/nrepl/impl/server.clj
@@ -57,7 +57,7 @@
     syms))
 
 (defn match [_alias->ns ns->alias query [sym-ns sym-name qualifier]]
-  (let [pat (re-pattern query)]
+  (let [pat (re-pattern (str/re-quote-replacement query))]
     (or (when (and (identical? :unqualified qualifier) (re-find pat sym-name))
           [sym-ns sym-name])
         (when sym-ns
