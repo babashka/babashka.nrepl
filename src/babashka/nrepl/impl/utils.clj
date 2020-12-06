13b732fbb16355c3e191680e8e5b93c610989cbc
Sameer Kolhar <kolhar730@gmail.com>
2020-12-06 09:34:38 +0530

    revert utils file changes

--- src/babashka/nrepl/impl/utils.clj
+++ src/babashka/nrepl/impl/utils.clj
@@ -7,21 +7,14 @@
 
 (set! *warn-on-reflection* true)
 
-;; TODO: should be based on this method here https://github.com/nrepl/nrepl/blob/0895caa7f0910c013014d554bbb262834a8408e4/src/clojure/nrepl/middleware/print.clj#L122
-(defn pretty-print [msg]
-  ;; TODO: insert pretty printing logic here
-  )
-
 (defn response-for [old-msg msg]
   (let [session (get old-msg :session "none")
         id (get old-msg :id "unknown")]
     (assoc msg "session" session "id" id)))
 
-(defn send [^OutputStream os msg {:keys [debug-send pprint]}]
+(defn send [^OutputStream os msg {:keys [debug-send]}]
   (when debug-send (prn "Sending" msg))
-  (if pprint
-    (write-bencode os (pretty-print msg))
-    (write-bencode os msg))
+  (write-bencode os msg)
   (.flush os))
 
 (defn send-exception [os msg ^Throwable ex {:keys [debug] :as opts}]
