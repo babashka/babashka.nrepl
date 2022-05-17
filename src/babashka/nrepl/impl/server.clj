ae4df03238584e95da04210d3b060af75866f422
Benj <Benjamin.Schwerdtner@gmail.com>
2022-05-17 13:59:43 +0200

    Support interop in complete
    Implement completion from imports
    
    Improve imports completion
    
    Delete whitespace
    
    Format complete
    
    Format complete
    
    Fix kondo unused warn
    
    Check if ns is there before calling ns-publics
    
    Respect pattern for import symbols
    
    Add ns part to interop completions

--- src/babashka/nrepl/impl/server.clj
+++ src/babashka/nrepl/impl/server.clj
@@ -5,6 +5,7 @@
             [bencode.core :refer [read-bencode]]
             [clojure.pprint :refer [pprint]]
             [clojure.string :as str]
+            [clojure.reflect]
             [sci.core :as sci])
   (:import [java.io InputStream PushbackInputStream EOFException BufferedOutputStream PrintWriter BufferedWriter Writer StringWriter]
            [java.net ServerSocket]))
@@ -144,6 +145,45 @@
               (when (re-find pat (str sym-ns "/" sym-name))
                 [sym-ns (str sym-ns "/" sym-name)]))))))
 
+(defn ns-imports->completions [ctx query-ns query]
+  (let [[ns-part name-part] (str/split query #"/")
+        resolved (sci/eval-string* ctx
+                                   (pr-str `(let [resolved# (resolve '~query-ns)]
+                                              (when-not (var? resolved#)
+                                                resolved#))))
+        pat (when name-part (re-pattern (java.util.regex.Pattern/quote name-part)))]
+    (when
+        resolved
+        (->>
+         (clojure.reflect/reflect resolved)
+         :members
+         (into
+          []
+          (comp
+           (filter (comp :static :flags))
+           (filter (comp :public :flags))
+           (filter (fn [{member-sym :name}]
+                     (or (not pat) (re-find pat (str member-sym)))))
+           (map
+            (fn [{:keys [name parameter-types]}]
+              [ns-part
+               (str ns-part "/" name)
+               (if parameter-types "static-method" "static-field")]))))))))
+
+(defn import-symbols->completions [imports query]
+  (let [pat (re-pattern (java.util.regex.Pattern/quote query))]
+    (doall
+     (sequence
+      (comp
+       (map key)
+       (filter
+        (fn [sym-name]
+          (re-find pat (str sym-name))))
+       (map (fn [class-name]
+              [nil (str class-name) "class"])))
+      imports))))
+
+
 (defn complete [rf result {:keys [ctx msg opts]}]
   (try
     (let [ns-str (get msg :ns)
@@ -153,6 +193,7 @@
         (if-let [query (or (:symbol msg)
                            (:prefix msg))]
           (let [has-namespace? (str/includes? query "/")
+                query-ns (when has-namespace? (symbol (first (str/split query #"/"))))
                 from-current-ns (fully-qualified-syms ctx (sci/eval-string* ctx "(ns-name *ns*)"))
                 from-current-ns (map (fn [sym]
                                        [(namespace sym) (name sym) :unqualified])
@@ -170,19 +211,29 @@
                 all-namespaces (->> (sci/eval-string* ctx (format "(all-ns)"))
                                     (map (fn [ns]
                                            [(str ns) nil :qualified])))
-                fully-qualified-names (when has-namespace?
-                                        (let [fqns (symbol (first (str/split query #"/")))
-                                              ns (get alias->ns fqns fqns)
-                                              syms (sci/eval-string* ctx (format "(keys (ns-publics '%s))" ns))]
-                                          (map (fn [sym]
-                                                 [(str ns) (str sym) :qualified])
-                                               syms)))
+                from-imports (when query-ns (ns-imports->completions ctx (symbol query-ns) query))
+                ns-found? (sci/eval-string* ctx (format "(find-ns '%s)" query-ns))
+                fully-qualified-names (when-not from-imports
+                                        (when (and has-namespace? ns-found?)
+                                          (let [ns (get alias->ns query-ns query-ns)
+                                                syms (sci/eval-string* ctx (format "(keys (ns-publics '%s))" ns))]
+                                            (map (fn [sym]
+                                                   [(str ns) (str sym) :qualified])
+                                                 syms))))
                 svs (concat from-current-ns from-aliased-nss all-namespaces fully-qualified-names)
                 completions (keep (fn [entry]
                                     (match alias->ns ns->alias query entry))
                                   svs)
-                completions (->> (map (fn [[namespace name]]
-                                        {"candidate" (str name) "ns" (str namespace) #_"type" #_"function"})
+                completions (concat completions from-imports)
+
+                import-symbols (import-symbols->completions (:imports @(:env ctx)) query)
+                completions (concat completions import-symbols)
+
+                completions (->> (map (fn [[namespace name type]]
+                                        (cond->
+                                            {"candidate" (str name)}
+                                            namespace (assoc "ns" (str namespace))
+                                            type (assoc "type" (str type))))
                                       completions)
                                  set)]
             (when (:debug opts) (prn "completions" completions))
